7bf594ad1110771f512957f6fb4b2b134b78ceb2
Melissa <meliswright925@gmail.com>
2019-11-24 22:37:51 -0800

    Third update

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,14 +4,20 @@ import csv
 budget_data = os.path.join("..","budget_data.csv")
 
 #calculate each of the following:
-
+def print_financialanalysis(budget_data):
+# For readability, it can help to assign your values to variables with descriptive nam
+    months = str(budget_data[0])
+    total_months = str(budget_data[0][0]])
+    profits = int(budget_data[1])
+    losses = int(budget_data[1])
+    
 #total number of months included in the dataset
-
+total_months = []
 #net total amount of "Profit/Losses" over the entire period
-
+total_amount = []
 #average of the changes in "Profit/Losses" over the entire period
-
+average_change = []
 #greatest increase in profits (date and amount) over the entire period
-
+average_change = int(wrestler_data[2])
 
 #greatest decrease in losses (date and amount) over the entire period
