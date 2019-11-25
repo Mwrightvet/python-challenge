f9c862be7c2fdb4877dc2eadd31f9cc20afa4db5
Melissa <meliswright925@gmail.com>
2019-11-24 22:35:35 -0800

    Second File update

---
+++ PyBank/main.py
@@ -0,0 +1,17 @@
+import os
+import csv
+
+budget_data = os.path.join("..","budget_data.csv")
+
+#calculate each of the following:
+
+#total number of months included in the dataset
+
+#net total amount of "Profit/Losses" over the entire period
+
+#average of the changes in "Profit/Losses" over the entire period
+
+#greatest increase in profits (date and amount) over the entire period
+
+
+#greatest decrease in losses (date and amount) over the entire period
