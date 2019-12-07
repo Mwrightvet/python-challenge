78d7301f3c713eaf649b0bcb1af8df9f6e40a7d4
Melissa <meliswright925@gmail.com>
2019-12-06 21:26:06 -0800

    Submitting Homework PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,21 +5,16 @@ budget_data = os.path.join('budget_data.csv')
 
 #total number of months included in the dataset
 total_months = 0
-#amount by month 
-amount = 0 
 #net total amount of "Profit/Losses" over the entire period
 total_amount = 0
-#average change by month 
-change_Average_monthly = 0 
 #change by month
-change_by_month = 0
+change_by_month = []
 #average of the changes in "Profit/Losses" over the entire period
 average_change = 0
 #greatest increase in profits (date and amount) over the entire period
 greatest_increase = 0 
 #greatest decrease in profits (date and amount) over the entire period
 greatest_decrease = 0 
-
 
 with open(budget_data, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -28,46 +23,53 @@ with open(budget_data, newline="") as csvfile:
     #Skipping the header 
     header = next(csvreader)
     #To see what I skipped . Confirm last command 
-    print("Skipped header:", header)
+    #print("Skipped header:", header)
+
+    #step 1 - Make a list of changes  
+
+    csvreader = list(csvreader)
     
-    #remember that it will run until stopped so tell it that when there is nothing there to stop.
-    #add if csv reader row is empty 
-    # stop = open ("budget_data.csv")
-    # for row in csv.reader(stop): 
-    # if not row: 
-    #     empty_rows += 1 
-    #     continue
-    # print row
-    #iterate   
-    for row in csvreader:
+ 
+    for index, row in enumerate(csvreader): 
+       # print(index, row)
         #print (row[1]) #to view first column 0, to view second it's 1
         #add to total_month 
         total_months += 1
         total_amount += float(row[1])
-        each_month_change = float(row[1]) - total_amount
-    print("Financial Analysis")
+        total_change = total_amount / total_months
+        
+        # AVERAGE CHANGE CALCULATION
+        # we need to keep track of the changes between two rows
+        # skip first row for average change
+        if (index != 0):
+            each_month_change = float(row[1]) - int(csvreader[index - 1][1])
+            change_by_month.append(each_month_change)
+      
+        #Remember to tell it that if it's comparing the 2nd change from the top 
+        #step 2 - sum the list and divide by the len()
+        #greatest increase in profits (date and amount) over the entire period
+        #step 1 - loop through the list 
+        #step 2 - find the highest value using the if/else (built in check) sum and length**
+    average_change = sum(change_by_month) / len(change_by_month)
+    greatest_increase = (max(change_by_month))
+    greatest_decrease = min(change_by_month)
+   
+
     print("----------------------------------------")
     print("Total Months:" , total_months)
-    print("Total Change:", average_change)
-#net total amount of "Profit/Losses" over the entire period
+    #print("Total Change:", total_change)
+    print("Average Change $:", average_change)
+    print("Greatest Increase $:", greatest_increase)
+    print("Greatest Decrease $:", greatest_decrease)
+    #net total amount of "Profit/Losses" over the entire period
     print("Total: $" , total_amount) 
 
-#average of the changes in "Profit/Losses" over the entire period
+output =(f"\nFinancial Analysis \n"
+        f"------------------------- \n"
+        f"Total Months: {total_months} \n"
+        f"Average Change: ${average_change}  \n"
+        f"Greatest Increase in Profits: ${greatest_increase}  \n"
+        f"Greatest Decrease in Profits: ${greatest_decrease}  ")
 
-#step 1 - loop through and make a list of changes, 
-#Remember to tell it that if it's comparing the 2nd change from the top 
-#step 2 - sum the list and divide by the len()
-
-   # for row in csvreader: 
-       # if amount != 0:
-        #change_by_month = int(row[1]) - amount 
-    #    change_Average_monthly += int(row[1]) - amount 
-
-#greatest increase in profits (date and amount) over the entire period
-#step 1 - loop through the list 
-#step 2 - find the highest value using the if/else (built in check) sum and length**
-        #if change_by_month < greatest_decrease:
-         #   greatest_decrease_is = row[0]
-          #  greatest_decrease = change_by_month
-    #amount = int(row[1])
-
+with open(os.path.join('financial_analysis.txt'), "w") as txtfile:
+    txtfile.write(output)
